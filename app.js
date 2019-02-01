62f7fffc86769e9fa07f8d69daff9813c850215e
Dim <dimbac@gamer.com>
2019-02-01 14:02:53 +0700

    Update bdgtController (parseFloat and prevent empty Desc & IsNan)

--- app.js
+++ app.js
@@ -66,7 +66,7 @@ var budgetController = (function(){
 //FIXME: UI CONTROLLER
 var UIController = (function(){
 
-    //we store it to var DOMstring, so if we want to change name at index.html, we can alway change here
+    //we store it to var DOMstring, so if we want to change name at index.html, we can always change here
     var DOMstrings = { 
         inputType: '.add__type',
         inputDescription: '.add__description',
@@ -81,7 +81,7 @@ var UIController = (function(){
             return {
                 type: document.querySelector(DOMstrings.inputType).value,  //will be inc or exp
                 description: document.querySelector(DOMstrings.inputDescription).value,
-                value: document.querySelector(DOMstrings.inputValue).value
+                value: parseFloat(document.querySelector(DOMstrings.inputValue).value) //for convert string to number, so we can calculate number
             };
         },
 
@@ -90,7 +90,6 @@ var UIController = (function(){
             var html, newHtml, element;
 
             //create HTML string with placeholder text
-
             if(type === 'inc'){
                 
                 element = DOMstrings.incomeContainer;
@@ -110,7 +109,6 @@ var UIController = (function(){
             newHtml = newHtml.replace('%value%', obj.value);
 
             //insert the HTML into the DOM
-
             document.querySelector(element).insertAdjacentHTML('beforeend', newHtml);
 
         },
@@ -118,7 +116,7 @@ var UIController = (function(){
         clearFields: function(){
             var fields, fieldsArray;
 
-            //Using the Array.prototype.slice without parameters it starts from the beginning and we convert the NodeList to an array. Then with call we specify the this as the fields variable. This is a trick.  The slice method thinks that we give it an array so it returns an array. We store the result in the fieldsArr and we can now use forEach method on NodeList. 
+            //simple: fields selecting element list html, but we want to try list into array. so we use Array function construct (Array.prototype), then 'Slice' return selected element as a new Array. and use 'call' for calling 'fields' which content are element. So fields(fieldsArray) is now an Array
 
             fields = document.querySelectorAll(DOMstrings.inputDescription + ', ' + DOMstrings.inputValue);
 
@@ -131,7 +129,7 @@ var UIController = (function(){
 
             fieldsArray[0].focus();
 
-            /* we can just use this easier method, but that above method is to show how to work with Nodelist
+            /* we can just use this easier method, but that above method is to show how to work with Nodelist into array
             document.querySelector(DOMstrings.inputDescription).value = "";
             document.querySelector(DOMstrings.inputValue).value = "";
             document.querySelector(DOMstrings.inputDescription).focus();
@@ -165,30 +163,40 @@ var controller = (function(budgetCtrl, UICtrl){
     };
 
 
+    var updateBudget = function(){
+        
+        //1. Calculate the budget 
+
+
+
+        //2. return bduget
+
+        //2. display budget to UI
+    }
 
     var ctrlAddItem = function(){
 
         var input, newItem;
 
         //1. Get the filed input data
-
         input = UICtrl.getInput();
 
-        //2. Add the item to the budget controller
+        if(input.description !==  "" && !isNaN(input.value) && input.value > 0){
+            //2. Add the item to the budget controller
+            newItem = budgetCtrl.addItem(input.type, input.description, input.value); //this use var input as argument(type des val), because in the function addItem that same argument
 
-        newItem = budgetCtrl.addItem(input.type, input.description, input.value); //this use var input as argument(type des val), because in the function addItem that same argument
+            //3. add the item to UI
+            UICtrl.addListItem(newItem, input.type);
 
-        //3. add the item to UI
-        UICtrl.addListItem(newItem, input.type);
+            //4. Clear the fields
+            UICtrl.clearFields();
 
-        //4. Clear the fields
-        UICtrl.clearFields();
-
-
-        //5. Calculate the budget 
-
-        //6. display budget to UI
-
+            //5. calculate and update budget
+            updateBudget();
+            
+        }else{
+            alert('Hmm.. Something went wrong!\n 1. Description is missing\n2. Value must above 0 or not empty')
+        }
     };
 
     return {
